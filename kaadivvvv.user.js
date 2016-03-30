6c861499c3359783c722b82c28f95dbe067bc6b2
Robotex <lightrobotex@yahoo.it>
2016-03-30 12:16:10 +0200

    Release 1.0.3
    Add https support

--- kaadivvvv.user.js
+++ kaadivvvv.user.js
@@ -3,12 +3,13 @@
 // @namespace   https://github.com/Robotex/
 // @description Mantieni il tuo Ad-Blocker attivo mentre ti guardi il tuo anime preferito
 // @author      Robotex
-// @version     1.0.2
+// @version     1.0.3
 // @license     GPL version 3; http://www.gnu.org/copyleft/gpl.html
 // @copyright   2016+, Robotex (https://github.com/Robotex/)
 // @homepage    https://github.com/Robotex/kaadivvvv/
 // @supportURL  https://github.com/Robotex/kaadivvvv/issues
 // @match       http://www.vvvvid.it/*
+// @match       https://www.vvvvid.it/*
 // @grant       none
 // ==/UserScript==
 
