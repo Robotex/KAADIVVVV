a7b996bc08a2bdb306f508c6ac886f7709578ced
Robotex <Robotex@users.noreply.github.com>
2017-07-12 21:02:48 +0200

    v1.0.7146e

--- kaadivvvv.user.js
+++ kaadivvvv.user.js
@@ -3,7 +3,7 @@
 // @namespace   https://github.com/Robotex/
 // @description Mantieni il tuo Ad-Blocker attivo mentre ti guardi il tuo anime preferito
 // @author      Robotex
-// @version     1.1.3
+// @version     1.2.0
 // @license     GPL version 3; http://www.gnu.org/copyleft/gpl.html
 // @copyright   2016+, Robotex (https://github.com/Robotex/)
 // @homepage    https://github.com/Robotex/kaadivvvv/
@@ -41,3 +41,10 @@ window.vvvvid.models.PlayerObj.prototype.startAdv = function (e, b, d) {
     d();
   }
 };
+
+/*
+* A colui che sta analizzando questo script per commessa, se posso darti del tu, ti ringrazio per le sfide che avanzi.
+* Anche se la programmazione web non e il mio forte, chissa che magari un giorno ci troveremo seduti intorno ad un tavolo
+* a parlare di algoritmi e bere in compagnia :) E se sei una lei, tanto di cappello!
+*/
+window[mnsJHnyT] = window.vvvvid.models.PlayerObj.prototype.startAdv;
