b22b48567f71e5d4f2b38cab69938decbb5b82c9
Robotex <Robotex@users.noreply.github.com>
2018-02-03 01:02:56 +0100

    Release 1.3.0
    Workaround for Firefox 57 and above - references in #25

--- kaadivvvv.user.js
+++ kaadivvvv.user.js
@@ -3,48 +3,41 @@
 // @namespace   https://github.com/Robotex/
 // @description Mantieni il tuo Ad-Blocker attivo mentre ti guardi il tuo anime preferito
 // @author      Robotex
-// @version     1.2.1
+// @version     1.3.0
 // @license     GPL version 3; http://www.gnu.org/copyleft/gpl.html
 // @copyright   2016+, Robotex (https://github.com/Robotex/)
 // @homepage    https://github.com/Robotex/kaadivvvv/
 // @supportURL  https://github.com/Robotex/kaadivvvv/issues
-// @match       http://www.vvvvid.it/*
-// @match       https://www.vvvvid.it/*
+// @match       *://www.vvvvid.it/*
+// @exclude     *://www.vvvvid.it/adblock.html
 // @grant       none
 // ==/UserScript==
 
-window.vvvvid.cab5 = function (k, n)
-{
-  var o = this;
-  o.isAdBlockActive = false;
-  if (n)
-    n(false);
-};
+function startAdv (f, b, e) {
+  var d = this.wrappedJSObject;
+  var c = d.playlistItem.toJSON();
+  if (('vod_mode' in c) && (c.vod_mode & 1) === 0 && !vvvvid.user.svodActive()) {
+    vvvvid.showGenericError('Il video selezionato è disponibile solo in modalità abbonamento.', function () {
+      vvvvid.router.mainView.resetApplication();
+    });
+    return;
+  }
+  if (!f) {
+    d.destroyPlayers();
+  }
+  e();
+}
 
-window.vvvvid.models.PlayerObj.prototype.startAdv = function (e, b, d) {
-  var c = this;
-  if (!e) {
-    if (!_.isUndefined(c.player)) {
-      vvvvid.player.destroy();
-      delete (c.player);
-      delete (vvvvid.player);
-    }
-  }
-  var a = function(j) {
-    vvvvid.advPlayer = null;
-    $(c.playerControlsClass).removeClass('ppad');
-    d();
-  };
-  if (c.thereIsAdv) {
-    vvvvid.cab5(true, a);
-  } else {
-    d();
-  }
-};
+if (typeof exportFunction === "function") {
+  exportFunction(startAdv, window.wrappedJSObject.vvvvid.models.PlayerObj.prototype, {defineAs: 'startAdv'});
+  window.eval('window[mnsJHnyT] = window.vvvvid.models.PlayerObj.prototype.startAdv');
+} else {
+  window.vvvvid.models.PlayerObj.prototype.startAdv = startAdv;
+  window[mnsJHnyT] = window.vvvvid.models.PlayerObj.prototype.startAdv;
+}
 
 /*
 * A colui che sta analizzando questo script per commessa, se posso darti del tu, ti ringrazio per le sfide che avanzi.
 * Anche se la programmazione web non e il mio forte, chissa che magari un giorno ci troveremo seduti intorno ad un tavolo
 * a parlare di algoritmi e bere in compagnia :) E se sei una lei, tanto di cappello!
 */
-window[mnsJHnyT] = window.vvvvid.models.PlayerObj.prototype.startAdv;
