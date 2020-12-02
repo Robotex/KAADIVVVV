a0840469f45bbedb98a8ca56f893e15904fd4613
Robotex <Robotex@users.noreply.github.com>
2020-12-02 23:28:57 +0100

    Release 1.7.0

--- kaadivvvv.user.js
+++ kaadivvvv.user.js
@@ -3,7 +3,7 @@
 // @namespace   https://github.com/Robotex/
 // @description Mantieni il tuo Ad-Blocker attivo mentre ti guardi il tuo anime preferito
 // @author      Robotex
-// @version     1.6.2
+// @version     1.7.0
 // @license     GPL-3.0-or-later; http://www.gnu.org/licenses/gpl-3.0.txt
 // @copyright   2016+, Robotex (https://github.com/Robotex/)
 // @homepage    https://github.com/Robotex/kaadivvvv/
@@ -33,25 +33,28 @@ function keyboardShortcuts(e) {
 }
 
 function checkAdv () {
-  this.play2Ads = false;
+  this.play4Ads = false;
+  if (typeof exportFunction === 'function') {
+    window.eval('window.vvvvid.imaPlayer.off(\'mediaError\')');
+  } else {
+    window.vvvvid.imaPlayer.off('mediaError');
+  }
 }
 
 if (typeof exportFunction === 'function') {
   exportFunction(checkAdv, window.wrappedJSObject.vvvvid.models.PlayerObj.prototype, {defineAs: 'checkAdv'});
-  window.eval('Object.defineProperty(window, \'emdHkdjhj\', { value: undefined, writable: false })');
-  window.eval('window[wnbshgd] = window.vvvvid.models.PlayerObj.prototype.checkAdv');
+  window.eval('Object.defineProperty(window, \'jhedogdjh\', { value: window.vvvvid.models.PlayerObj.prototype.checkAdv, writable: false })');
+  window.eval('window[qnejh7HJ] = window.vvvvid.models.PlayerObj.prototype.checkAdv');
   window.eval('window.vvvvid.lastPlayedPreCommunityTime = Number.POSITIVE_INFINITY');
-  window.eval('window.nopub = true;');
   window.eval('$(document).keydown(' + keyboardShortcuts + ')');
 } else {
   window.vvvvid.models.PlayerObj.prototype.checkAdv = checkAdv;
-  Object.defineProperty(window, 'emdHkdjhj', { value: undefined, writable: false });
-  window[wnbshgd] = window.vvvvid.models.PlayerObj.prototype.checkAdv;
+  Object.defineProperty(window, 'jhedogdjh', { value: window.vvvvid.models.PlayerObj.prototype.checkAdv, writable: false });
+  window[qnejh7HJ] = window.vvvvid.models.PlayerObj.prototype.checkAdv;
   window.vvvvid.lastPlayedPreCommunityTime = Number.POSITIVE_INFINITY;
-  window.nopub = true;
   $(document).keydown(keyboardShortcuts);
 }
-window.sessionStorage.logoPlayed = true;
+window.sessionStorage.vvvvid.logoPlayed = '1';
 
 /**
 * A colui che sta analizzando questo script per commessa, se posso darti del tu, ti ringrazio per le sfide che avanzi.
